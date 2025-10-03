224a14a5e9170c883c1802900e1728ae876b96ef
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-10-03 11:57:06 -0600

    I've adapted the main to the new features

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from datetime import date
 from model.Data.settings import settings
 from model.Data.universityData import universityData
 from model.pdfCoverGenerator import pdfCoverGenerator
+from model.pdfMerger import pdfMerger
+from view.pathFiles import pathFiles
 
 
 def main():
-    a = pdfCoverGenerator(settings, universityData, "Ejmplo Lorem", "Materia Inutil de Vergara", "Balatrexis Nava Moya", "Magdiel Market", date.today())
+    
+    ActivityName = "Actividad inutil"
+    EEName = "Materia Inutil"
+    NameUser = "Balatrexis Nava Moya"
+    FacilitatorName = "Magdiel Market"
+    
+    a = pdfCoverGenerator(settings, universityData, ActivityName, EEName, NameUser, FacilitatorName, date.today())
     a.generate()
-   
+    
+    pdflist = pathFiles.selectDocFiles()
+    print(type(pdflist))
+    pdflist = list(pdflist)
+    pdflist.insert(0, "cover.pdf")
+    print(pdflist)
+    pdfMerger(pdflist)
+  
 if __name__ == '__main__':
     main()
